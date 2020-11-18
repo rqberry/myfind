1f3d866ec5b28a25e9d3a8d9847a67bbc33cdd1e
rqberry <qberry.ryan@gmail.com>
2020-11-18 11:53:25 -0800

    some myfind edits

--- myfind.cc
+++ myfind.cc
@@ -1,8 +1,24 @@
+#include <fstream>
 #include <iostream>
 #include <filesystem>
 
+using fs = std::filesystem;
+
+fs::path START_PATH = fs::current_path();
+
+//iterate throught the children of a directory
+void print_dir(fs::path directory) {
+  (void) directory;
+}
+
+
 int main(int arc, char **argv) {
-
-
-    return 0;
+  fs::current_path(fs::temp_directory_path());
+  fs::create_directories("sandbox/a/b");
+  std::ofstream("sandbox/file1.txt");
+  fs::create_symlink("a", "sandbox/syma");
+  for(auto& p: fs::recursive_directory_iterator("sandbox"))
+      std::cout << p.path() << '\n';
+  fs::remove_all("sandbox");
+  return 0;
 }

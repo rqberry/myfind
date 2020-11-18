f6fab6e33b34a4a709a3ac164d974ae542e39143
rqberry <qberry.ryan@gmail.com>
2020-11-18 12:12:15 -0800

    basic find working

--- myfind.cc
+++ myfind.cc
@@ -13,12 +13,11 @@ void print_dir(fs::path directory) {
 
 
 int main(int argc, char **argv) {
-  fs::current_path(fs::temp_directory_path());
-  fs::create_directories("sandbox/a/b");
-  std::ofstream("sandbox/file1.txt");
-  fs::create_symlink("a", "sandbox/syma");
-  for(auto& p: fs::recursive_directory_iterator("sandbox"))
-      std::cout << p.path() << '\n';
-  fs::remove_all("sandbox");
+  std::cout<<".\n";
+  for(auto& p: fs::recursive_directory_iterator(".")){
+      auto s = (std::string) p.path();
+      std::string p_s = s.substr(1,s.length()-1);
+      std::cout <<"." <<p_s << '\n';
+    }
   return 0;
 }

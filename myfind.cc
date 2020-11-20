35b512664034ffef2301f4bcdfb2ab41dc21887c
rqberry <qberry.ryan@gmail.com>
2020-11-20 10:04:18 -0800

    -L working

--- myfind.cc
+++ myfind.cc
@@ -1,7 +1,8 @@
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include <filesystem>
+#include <fstream>      // needed for filesystem
+#include <iostream>     //for cout
+#include <vector>       // for vector
+#include <filesystem>   // for the the thing that does our project for us lol
+#include <algorithm>    //for std::find
 
 namespace fs = std::filesystem;
 
@@ -9,7 +10,7 @@ fs::path START_PATH = fs::current_path();
 
 
 int main(int argc, char **argv) {
-  fs::directory_options.follow_directory_symlink = false;
+
 
 
   std::vector<std::string> arg_list;
@@ -17,11 +18,15 @@ int main(int argc, char **argv) {
 
 
 
+  fs::directory_options hyphen_L = (std::find(arg_list.begin(),arg_list.end(),"-L") != arg_list.end()) ?
+    fs::directory_options::follow_directory_symlink : fs::directory_options::none;
+
   std::cout<<".\n";
-  for(auto& p: fs::recursive_directory_iterator(".")){
+  for (auto& p : fs::recursive_directory_iterator(".",hyphen_L))
+  {
       auto s = (std::string) p.path();
       std::string p_s = s.substr(1,s.length()-1);
       std::cout <<"." <<p_s << '\n';
-    }
+  }
   return 0;
 }

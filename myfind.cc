7c7e565b5c455fb5fd10fa82db54d7c8e5355d32
rqberry <qberry.ryan@gmail.com>
2020-11-27 08:31:17 -0800

    segfault now again

--- myfind.cc
+++ myfind.cc
@@ -45,8 +45,11 @@ int execute(const char *pathname, std::vector<std::string> arg_list) {
 */
 
 
-void main_loop_r(fs::directory_entry dir, fs::directory_options L_token, std::string name_token, bool mtime_token) {
-  fs::directory_iterator dir_iterator(dir.path(),L_token);
+void main_loop_r(fs::path top_p, fs::directory_options L_token, std::string name_token, bool mtime_token) {
+  /*
+  (void) top_p;
+  const std::string ass =  ".";
+  fs::directory_iterator dir_iterator(ass,L_token);
   for(auto& p : dir_iterator)
   {
     //this is for print
@@ -61,7 +64,7 @@ void main_loop_r(fs::directory_entry dir, fs::directory_options L_token, std::st
     std::time_t mtime_token_comp = now;
     if (mtime_token) mtime_token_comp = std::chrono::system_clock::to_time_t(fs::last_write_time(p.path()));
 
-    //the holy mr.if statment
+    //The Holy Mr. If Statment
     if (
       (*(--p.path().end())).compare(name_token_comp) == 0 &&
       mtime_token_comp >= now - 86400 // &&
@@ -72,6 +75,7 @@ void main_loop_r(fs::directory_entry dir, fs::directory_options L_token, std::st
     if (p.is_directory())
       main_loop_r(p, L_token, name_token, mtime_token);
     }
+    */
 }
 
 
@@ -84,16 +88,30 @@ int main(int argc, char **argv) {
   //for -name, we find name if it exists and then get the argument "name_token" for future usage
   auto name_iter = std::find(arg_list.begin(),arg_list.end(),"-name");
 
-  //TODO: print to error for no -name specification "find: missing argument to `-name'"
+  std::string name_token = "";
 
   //getting name token, this is pretty self explanatory. if the name iter points to the end, there is no name_token for us to get
-  // otherwise, we get whatever's after... idk if thi
-  std::string name_token = (name_iter != arg_list.end()) ? arg_list[std::distance(arg_list.begin(),name_iter)+1] : "";
+  // otherwise, we get whatever's after... idk if this works????????? shouldn't it be checking whatever's after name_iter?
+  // i think this has a bug, TODO: check if the following is right
+  try{
+    name_token = (name_iter != arg_list.end()) ? arg_list[std::distance(arg_list.begin(),name_iter)+1] : "";
+  }
+  catch (int e){
+    //TODO: print to error for no -name specification "find: missing argument to `-name'"
+
+    //if the above fails, it means there's an array out of bounds error where arglist don't have arglist[stuff+1]
+    //aka, there is no argument for -name!!!
+    std::cout<<"find: missing argument to `-name'"<<std::endl;
+    return 1;
+  }
+
   //TODO: print to error & exit gracefully
   if (name_token != "" && name_token.substr(0,1) == "/") {
       std::cout << "find: warning: ‘-name’ matches against basenames only, but the given pattern contains a directory separator (‘/’), thus the expression will evaluate to false all the time.  Did you mean ‘-wholename’?" << std::endl;
       return 1;
   }
+
+
 
   bool mtime_token = false;
   //for -mtime
@@ -116,7 +134,8 @@ int main(int argc, char **argv) {
   }
 
   //for -type
-  //TODO: WHAT IF THERE ARE MULTIPLE '-type's !?!?!?!?
+  //TODO: WHAT IF THERE ARE MULTIPLE '-type's !?!?!?!? <- vector of types!!!!!
+  //we cant use std::find
   //auto type_iter = std::find(arg_list.begin(),arg_list.end(),"-type");
   //char type_token = (type_iter != arg_list.end()) ? arg_list[type_iter+1] : (char) 0;
 
@@ -130,7 +149,7 @@ int main(int argc, char **argv) {
       std::cout << "." << std::endl;
   }
 
-  main_loop_r(fs::directory_entry("."),L_token,name_token,mtime_token);
+  main_loop_r(fs::current_path(),L_token,name_token,mtime_token);
 
   return 0;
 }
